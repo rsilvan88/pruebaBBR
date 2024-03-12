6d93c9cefbdefedbcbff6f9a67bc7108665ff7cf
Rodrigo Silva Navea <rodrigo.silva@evertecinc.com>
2024-03-12 17:57:02 -0300

    update cors local

--- src/main/java/com/evertec/springboot2/crud/configuration/CorsConfig.java
+++ src/main/java/com/evertec/springboot2/crud/configuration/CorsConfig.java
@@ -7,23 +7,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    @SuppressWarnings("null")
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/v1/tareas/**")
-                .allowedOrigins("https://ambitious-ocean-092d6240f.5.azurestaticapps.net")
+                .allowedOrigins("https://ambitious-ocean-092d6240f.5.azurestaticapps.net","http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
         
         registry.addMapping("/auth")
-                .allowedOrigins("https://ambitious-ocean-092d6240f.5.azurestaticapps.net")
+                .allowedOrigins("https://ambitious-ocean-092d6240f.5.azurestaticapps.net","http://localhost:3000")
                 .allowedMethods("POST")
                 .allowedHeaders("*")
                 .allowCredentials(true);
         
         registry.addMapping("/validateAuth")
-                .allowedOrigins("https://ambitious-ocean-092d6240f.5.azurestaticapps.net")
+                .allowedOrigins("https://ambitious-ocean-092d6240f.5.azurestaticapps.net","http://localhost:3000")
                 .allowedMethods("POST")
                 .allowedHeaders("*")
                 .allowCredentials(true);
